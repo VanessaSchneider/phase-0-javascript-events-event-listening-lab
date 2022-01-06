1a1bc58d9104235846c9f321bff04e9d69444cc5
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-06 13:15:35 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ function clickAlert() {
 
 input.addEventListener('click', clickAlert);
 }
+
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
